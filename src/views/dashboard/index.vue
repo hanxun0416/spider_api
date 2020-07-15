229e1660073130d16926af37dfd3d4808391ad07
unknown <374677308@qq.com>
2020-07-15 21:37:12 +0800

    first commit

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="dashboard-container">
+    123
+   <component :is="currentRole" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import adminDashboard from "./admin";
+
+export default {
+  name: "Dashboard",
+  computed: {
+    ...mapGetters(["name"])
+  },
+    data() {
+    return {
+      currentRole: 'adminDashboard'
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
